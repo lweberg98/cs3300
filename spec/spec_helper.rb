dda27637443cfafd8747324863e5d329bc87aa73
root <lweberg98@gmail.com>
2022-04-20 18:12:53 +0000

    iteration 5.2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -104,5 +104,6 @@ SimpleCov.start 'rails' do
   add_filter 'app/channels/application_cable/connection.rb'
   add_filter 'app/mailers/application_mailer.rb'
   add_filter 'app/jobs/application_job.rb'
+  add_filter 'app/mailers/application_mailer.rb'
 end
 
